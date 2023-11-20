449a2d41040150847c058790a67500e746cca529
ljh0608 <m98m@catholic.ac.kr>
2023-11-20 12:49:21 +0900

    init: styled-components - globalStyle 적용

---
+++ EQL-Client-Project/src/styles/globalStyle.ts
@@ -0,0 +1,37 @@
+import * as styled from 'styled-components';
+
+import { reset } from 'styled-reset';
+
+const GlobalStyle = styled.createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0 auto;
+
+    font-size: 62.5%;
+  }
+
+  a {
+    cursor: pointer;
+
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    font: inherit;
+  }
+
+  select {
+    cursor: pointer;
+  }
+`;
+export default GlobalStyle;
