2832c6d607fe651e682d305058dbb03a4012fd97
ljh0608 <m98m@catholic.ac.kr>
2023-11-23 14:03:50 +0900

    style: footer layout 제작

--- EQL-Client-Project/src/components/footer/Footer.style.tsx
+++ EQL-Client-Project/src/components/footer/Footer.style.tsx
@@ -3,5 +3,31 @@ export const FooterContainer = styled.div`
   display: flex;
   width: 100%;
   height: 40rem;
-  background-color: #a6c960;
+  /* background-color: #a6c960; */
+  border-top: 1px solid ${({ theme }) => theme.colors.grey4};
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 45%;
+  padding-top: 4.4rem;
+  padding-bottom: 3.9rem;
+  padding-left: 3.6rem;
+`;
+
+export const InfoMainText = styled.h2`
+  ${({ theme }) => theme.fonts.body9};
+`;
+
+export const MenuContainer = styled.div<{ $sequence: string }>`
+  display: flex;
+  flex-direction: column;
+  width: ${({ $sequence }) => ($sequence === 'middle' ? '32%' : '23%')};
+  padding: 4.2rem 3.1rem 3.1rem 3.1rem;
+`;
+
+export const FooterHr = styled.hr`
+  color: ${({ theme }) => theme.colors.grey4};
 `;
--- EQL-Client-Project/src/components/footer/Footer.tsx
+++ EQL-Client-Project/src/components/footer/Footer.tsx
@@ -1,6 +1,17 @@
 import * as S from './Footer.style';
+
 const Footer = () => {
-  return <S.FooterContainer>Footer</S.FooterContainer>;
+  return (
+    <S.FooterContainer>
+      <S.FooterContainer>
+        <S.InfoContainer> </S.InfoContainer>
+        <S.FooterHr />
+        <S.MenuContainer $sequence={'middle'}></S.MenuContainer>
+        <S.FooterHr />
+        <S.MenuContainer $sequence={'last'}></S.MenuContainer>
+      </S.FooterContainer>
+    </S.FooterContainer>
+  );
 };
 
 export default Footer;
