0cb62aa575e3078554a791f3368a2efffa5c33d1
SynthiaLee <seanne0801@gmail.com>
2023-11-28 03:43:31 +0900

    feat/#33: 탑 버튼 스크롤에 따라 렌더링

--- EQL-Client-Project/src/components/topButton/TopBtn.tsx
+++ EQL-Client-Project/src/components/topButton/TopBtn.tsx
@@ -1,19 +1,34 @@
 import * as S from './TopBtn.style';
 import { IcArrowUpLinear } from '../../assets/svgs/0_icons';
-
-const ScrollToTop = () => {
-  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-};
-
-// window.addEventListener("scroll", function () {
-//     S.TopBtnContainer.style.opacity = window.scrollY / 2000;
-//   });
+import { useEffect, useState } from 'react';
 
 const TopBtn = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  //윈도우의 top에 있을때엔 topBtn이 보이지 않도록 설정
+  useEffect(() => {
+    const RenderTopBtn = () => {
+      if (window.scrollY > 100) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    };
+    window.addEventListener('scroll', RenderTopBtn);
+  }, []);
+
+  const ScrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
   return (
-    <S.TopBtnContainer onClick={ScrollToTop}>
-      <IcArrowUpLinear />
-    </S.TopBtnContainer>
+    <>
+      {showTopBtn && (
+        <S.TopBtnContainer onClick={ScrollToTop}>
+          <IcArrowUpLinear />
+        </S.TopBtnContainer>
+      )}
+    </>
   );
 };
 
