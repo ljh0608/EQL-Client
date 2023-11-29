5f86882b92aed0ebe3f11cc897e3b17ee0c9e790
ljh0608 <m98m@catholic.ac.kr>
2023-11-29 16:50:24 +0900

    design: 유의사항 컴포넌트

---
+++ EQL-Client-Project/src/components/order/Notice/Notice.style.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+export const NoticeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.4rem;
+
+  width: 76.2rem;
+  height: 35.1rem;
+  margin-top: 2.6rem;
+  margin-bottom: 5.1rem;
+`;
+
+export const HeadingText = styled.p`
+  ${({ theme }) => theme.fonts.body5};
+
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const NoticeContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  width: 77.3rem;
+  height: 27rem;
+`;
+
+export const Content = styled.p`
+  ${({ theme }) => theme.fonts.body9};
+
+  color: ${({ theme }) => theme.colors.grey2};
+`;
+
+export const Temp = styled.span`
+  ${({ theme }) => theme.fonts.body9};
+
+  color: ${({ theme }) => theme.colors.grey2};
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+`;
