fa279387f1cab1eb0c7419e797ff50b16dda1d10
ljh0608 <m98m@catholic.ac.kr>
2023-12-02 00:00:52 +0900

    fix: image arr 사용

---
+++ EQL-Client-Project/src/constants/HomeContent.ts
@@ -0,0 +1,11 @@
+import { ImgPost1, ImgPost2, ImgPost3, ImgPost4, ImgPost5, ImgPost7 } from '../assets/Images';
+
+export const PRODUCTPOSTIMG = [
+  '0번째 이미지 없음',
+  ImgPost1,
+  ImgPost2,
+  ImgPost3,
+  ImgPost4,
+  ImgPost5,
+  ImgPost7,
+];
