59d47a737eac70801da908bd10bc9b830ce0b2f1
ljh0608 <m98m@catholic.ac.kr>
2023-12-01 14:01:36 +0900

    fix: order list render

--- EQL-Client-Project/src/components/order/OrderProductInfo/OrderProductInfo.tsx
+++ EQL-Client-Project/src/components/order/OrderProductInfo/OrderProductInfo.tsx
@@ -14,39 +14,45 @@ type OrderProductInfoTypes = {
       amount: number;
       itemName: string;
       brandName: string;
-      totalItemPrice: number;
+      getTotalItemPrice: number;
     }[];
   };
 };
 const OrderProductInfo = ({ itemInfo }: OrderProductInfoTypes) => {
-  const { totalItemsAmount, totalItemsPrice, itemSummaries } = itemInfo;
-
+  const { totalItemsAmount, itemSummaries } = itemInfo;
+  console.log(itemSummaries);
   return (
     <S.OrderWrapper>
       <OrderHeader orderCount={totalItemsAmount}>주문 상품 정보</OrderHeader>
-      <S.OrderInfoWrapper>
-        <S.TempImgDiv></S.TempImgDiv>
-        <S.OrderInfoContiner>
-          <S.BrandText>{itemSummaries[0].brandName}</S.BrandText>
-          <S.Body7>[11/08(수) 예약배송] {itemSummaries[0].itemName} (BLACK)</S.Body7>
-          <Spacing $spacing={'1.2'}></Spacing>
-          <S.FlexColumnBox>
-            <S.Body8>옵션</S.Body8>
-            <S.Body9>BLACK / M</S.Body9>
-          </S.FlexColumnBox>
-          <Spacing $spacing={'0.5'}></Spacing>
-          <S.FlexColumnBox>
-            <S.Body8>수량 </S.Body8>
-            <S.Body9>{totalItemsAmount}개</S.Body9>
-          </S.FlexColumnBox>
-          <Spacing $spacing={'1.4'}></Spacing>
-          <S.PriceWrapper>
-            <S.Body8>가격</S.Body8>
-            <S.Price>{regPrice(totalItemsPrice)}</S.Price>
-          </S.PriceWrapper>
-        </S.OrderInfoContiner>
-      </S.OrderInfoWrapper>
-      <LightHr />
+
+      {itemSummaries.map((item, index) => {
+        return (
+          <S.OrderInfoWrapper key={index}>
+            <S.TempImgDiv></S.TempImgDiv>
+            <S.OrderInfoContiner>
+              <S.BrandText>{item.brandName}</S.BrandText>
+              <S.Body7>[11/08(수) 예약배송] {item.itemName} (BLACK)</S.Body7>
+              <Spacing $spacing={'1.2'}></Spacing>
+              <S.FlexColumnBox>
+                <S.Body8>옵션</S.Body8>
+                <S.Body9>BLACK / M</S.Body9>
+              </S.FlexColumnBox>
+              <Spacing $spacing={'0.5'}></Spacing>
+              <S.FlexColumnBox>
+                <S.Body8>수량 </S.Body8>
+                <S.Body9>{item.amount}개</S.Body9>
+              </S.FlexColumnBox>
+              <Spacing $spacing={'1.4'}></Spacing>
+              <S.PriceWrapper>
+                <S.Body8>가격</S.Body8>
+                <S.Price>{regPrice(item.getTotalItemPrice)}</S.Price>
+              </S.PriceWrapper>
+              <Spacing $spacing={'2'}></Spacing>
+              <LightHr />
+            </S.OrderInfoContiner>
+          </S.OrderInfoWrapper>
+        );
+      })}
     </S.OrderWrapper>
   );
 };
