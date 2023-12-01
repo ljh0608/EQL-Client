64a053455ac6c502dbefe340d2ee7b5036aede87
SynthiaLee <seanne0801@gmail.com>
2023-12-01 21:21:49 +0900

    chore/#58: 불필요 주석 삭제

--- EQL-Client-Project/src/assets/images/index.ts
+++ EQL-Client-Project/src/assets/images/index.ts
@@ -43,25 +43,3 @@ export { ImgPost7 };
 export { ImgPost8 };
 export { ImgPost9 };
 export { ImgPost10 };
-// import {  RecommendFirstImg } from './RecommendFirstImg.png';
-// import {  RecommendSecondImg } from './RecommendSecondImg.png';
-// import {  CarouselFirstImg } from './CarouselFirstImg.png';
-// import {  CarouselSecondImg }
-// import {  CarouselThirdImg }
-// import {  ImgPost1 } from './ImgPost1.png';
-// import {  ImgPost2 } from './ImgPost2.png';
-// import {  ImgPost3 } from './ImgPost3.png';
-// import {  ImgPost4 } from './ImgPost4.png';
-// import {  ImgPost5 } from './ImgPost5.png';
-// import {  ImgRec1 } from './ImgRec1.png';
-// import {  ImgRec2 } from './ImgRec2.png';
-// import {  ImgRec3 } from './ImgRec3.png';
-// import {  ImgRec4 } from './ImgRec4.png';
-// import {  ImgRec5 } from './ImgRec5.png';
-// import {  ImgRec6 } from './ImgRec6.png';
-// import {  ImgRec7 } from './ImgRec7.png';
-// import {  ImgPost7 } from './ImgPost7.png';
-// import {  ImgPost8 } from './ImgPost8.png';
-// import {  ImgPost9 } from './ImgPost9.png';
-// import {  ImgPost10 } from './ImgPost10.png';
-// import {  ImgPost11 } from './ImgPost11.png';
